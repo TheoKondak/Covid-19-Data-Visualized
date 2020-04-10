1986addc14d3eaa434833224fd6877731835dba7
TheoKondak <thkondak@gmail.com>
2020-04-10 03:17:27 +0200

    Updates

--- precache-manifest.0d2d5b59c7a675dd29be2f6e7476852a.js
+++ precache-manifest.0d2d5b59c7a675dd29be2f6e7476852a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a705fa58c2c28f8477a2cca50648e789",
+    "revision": "c835b73e6493e4549c60355b3e88e926",
     "url": "/Covid-19-Data-Visualized/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Covid-19-Data-Visualized/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "b61fee9f7ec6e9cedb9a",
-    "url": "/Covid-19-Data-Visualized/static/css/main.c29f8ce0.chunk.css"
+    "revision": "5c671ac7d31aea47e87f",
+    "url": "/Covid-19-Data-Visualized/static/css/main.5b774e49.chunk.css"
   },
   {
     "revision": "b930dd0784e293d48bf7",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Covid-19-Data-Visualized/static/js/2.8a464d71.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b61fee9f7ec6e9cedb9a",
-    "url": "/Covid-19-Data-Visualized/static/js/main.728d03d5.chunk.js"
+    "revision": "5c671ac7d31aea47e87f",
+    "url": "/Covid-19-Data-Visualized/static/js/main.b92a191b.chunk.js"
   },
   {
     "revision": "246a9b0022f24d0c524b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid-19-Data-Visualized/precache-manifest.aec33027a04ec802a54d94bbbf0867b7.js"
+  "/Covid-19-Data-Visualized/precache-manifest.0d2d5b59c7a675dd29be2f6e7476852a.js"
 );
 
 self.addEventListener('message', (event) => {
